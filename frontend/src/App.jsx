9f7fbe5d14a95b62e28f369b3d74495705b06fd8
ShreeLite <shreemcream@gmail.com>
2025-06-22 14:10:42 +0000

    WIP:LOCAL PORT TO UBUNTU

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Route,createBrowserRouter,createRoutesFromElements,RouterProvider} from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div>App1</div>
+  )
+}
+
+export default App
